60d1ba76654b7aef2c45fe1a3006159ed91365cf
heli17p <heli17p@gmail.com>
2025-12-11 20:46:38 +0100

    Fix Tracking

--- utils/media.ts
+++ utils/media.ts
@@ -1,4 +1,3 @@
-
-// A minimal, valid WebM video file (black screen/silent) encoded in Base64.
-// Used to keep the screen awake on mobile devices via the HTML5 Video hack.
-export const NO_SLEEP_VIDEO_WEBM = "data:video/webm;base64,GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYX4BNQI0VSalmAAAAAAAVsalmAAAAAAAFqUbfQCoCWGtzaHU0gQFC94EBQreBAkF2gQlMdm9yYmlzQoUBAAAAAAAAE4EDAQAAAAAAACqbAQ4AAAAAAABpYgAAAAAAANF7AAAAAAAAVrp/";
+// Datei kann gelöscht werden, da wir WakeLock API nutzen.
+// Wenn sie bleiben muss:
+export const NO_SLEEP_VIDEO_WEBM = "";
